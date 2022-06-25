9a37614cabf57691eff7de3f27af94d50e9bc1c6
Derick J. Carcamo <derick_vlk_@hotmail.es>
2022-06-25 15:59:56 -0600

    Landing page

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const app = express();
 const port = 5000;
 const path = require('path');
 
+app.use(express.static("public"))
+app.use("./css", express.static(__dirname+"public/css"))
+app.use("./js",express.static(__dirname+"public/js"))
+app.use("./img",express.static(__dirname+"public/img"))
+
+app.set('views','./views')
+app.set('view engine','ejs')
 
 app.get('/', (req,res) =>{
-    res.sendFile('index.html', {root: './public/'})
+    res.render('index',{text:'Hola mundo!'})
 })
 
 app.listen(port, () => {
