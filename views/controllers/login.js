eb12d05105b4878f960a5231a9ec111edcb061ab
Derick J. Carcamo <derick_vlk_@hotmail.es>
2022-06-27 14:55:41 -0600

    Login finalizado con conexion en firebase y el backend

--- views/controllers/login.js
+++ views/controllers/login.js
@@ -1,13 +1,21 @@
-//const jsdom = require ( "jsdom" );
-//const dom = new jsdom.JSDOM("");
-//const $ = require ( 'jquery' )( dom.window );
-import jsdom from "jsdom";
-const dom = new jsdom.JSDOM("");
-import $ from ( 'jquery' )( dom.window );
-
 var email;
 var password;
 
 $("#Signin").click(()=>{
-    console.log("Intentaste logear");
+    var data =  `emailname=${$('#txt_email').val()}&password=${$('#pwd_pass').val()}`;
+    $.ajax({
+        url:"http://localhost:5001/AuthenticationFirabase/login",
+        method:"post",
+        data: data,
+        dataType: "json",
+        success:(response)=>{
+            if (response){
+                let uid = response.uid;
+                console.log('uid '+response.uid);
+            }
+        },error:(error)=>{
+            console.log('HOLA');
+            console.log(error);
+        }   
+    })
 })
